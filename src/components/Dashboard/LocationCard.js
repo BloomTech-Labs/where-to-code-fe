7d2a3bf82b3aa018767df186c5fbd85d58454353
AceIsHuman <menoman1995@yahoo.com>
2020-04-18 19:20:39 -0500

    refactor to use location from visit

--- src/components/Dashboard/LocationCard.js
+++ src/components/Dashboard/LocationCard.js
@@ -1,16 +1,25 @@
 import React from "react";
+import moment from "moment";
 
-const LocationCard = ({ location }) => (
-  <section className="location-listing">
-    <section>
-      <b>{location.name}</b>
-      <br />
-      <span>{location.address}</span>
+const LocationCard = ({ location, saved, visit }) => {
+  const { name, address } = location || visit.location;
+  return (
+    <section className="location-listing">
+      <section>
+        <b>{name}</b>
+        <br />
+        <span>{address}</span>
+        <br />
+      </section>
+      <p>
+        {!!saved ? (
+          <i className="fas fa-heart"></i>
+        ) : (
+          <i className="fas fa-long-arrow-alt-right"></i>
+        )}
+      </p>
     </section>
-    <p>
-      <i className="fas fa-heart"></i>
-    </p>
-  </section>
-);
+  );
+};
 
 export default LocationCard;
