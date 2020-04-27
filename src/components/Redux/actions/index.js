9378289ceb78538a454b69b5bdccb391db9872f6
AceIsHuman <menoman1995@yahoo.com>
2020-04-27 00:10:11 -0500

    add map to redux state

--- src/components/Redux/actions/index.js
+++ src/components/Redux/actions/index.js
@@ -1,15 +1,21 @@
 import axiosWithAuth from "../../../Helpers/axiosWithAuth";
 
+// Authentication
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const SIGN_OUT = "SIGN_OUT";
 
+// Google Maps
 export const UPDATE_PLACE = "UPDATE_PLACE";
+export const UPDATE_MAP = "UPDATE_MAP";
+
+// User Dashboard
 export const UPDATE_SAVED_LOCATIONS = "UPDATE_SAVED_LOCATIONS";
-export const UPDATE_USER_VISITS = "UPDATE_USER_VISITS";
-
-export const REMOVE_USER_VISIT = "REMOVE_USER_VISIT";
 export const REMOVE_SAVED_LOCATION = "REMOVE_SAVED_LOCATION";
+export const UPDATE_USER_VISITS = "UPDATE_USER_VISITS";
+export const REMOVE_USER_VISIT = "REMOVE_USER_VISIT";
 
+// ACTIONS
+// Authentication
 export const login = (e, creds, history) => dispatch => {
   e.preventDefault();
   axiosWithAuth()
@@ -68,6 +74,7 @@ export const signout = (e, history) => dispatch => {
   dispatch({ type: SIGN_OUT });
 };
 
+// Google Maps
 export const updatePlace = place => dispatch => {
   dispatch({ type: UPDATE_PLACE, payload: place });
 };
@@ -81,6 +88,12 @@ export const getSavedLocations = () => dispatch => {
     .catch(err => console.error(err.message));
 };
 
+export const updateMap = map => ({
+  type: UPDATE_MAP,
+  payload: map
+})
+
+// User Dashboard
 export const getUserVisits = () => dispatch => {
   axiosWithAuth()
     .get("/locations/visited/")
