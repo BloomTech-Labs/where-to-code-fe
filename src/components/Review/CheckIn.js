76ee48ddb56b0c908a6dc4ad5293b808f932d459
AceIsHuman <menoman1995@yahoo.com>
2020-04-28 05:34:36 -0500

    begin creating CheckIn and FavoriteLocation components

---
+++ src/components/Review/CheckIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckIn = props => {
+  return <span>Check in</span>;
+};
+
+export default CheckIn;
---
+++ src/components/Review/FavoriteLocation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FavoriteLocation = props => {
+  return <span>Favorite Location</span>;
+};
+
+export default FavoriteLocation;
