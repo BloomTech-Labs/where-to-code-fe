af93eb3be68534d5c6d928f853c9f2cec46f2bec
AceIsHuman <menoman1995@yahoo.com>
2020-04-28 02:52:31 -0500

    added placeholder for locations if none exist

--- src/components/Dashboard/SavedLocations.js
+++ src/components/Dashboard/SavedLocations.js
@@ -1,20 +1,35 @@
 import React from "react";
 import LocationCard from "./LocationCard";
+import styled from "styled-components";
 
 const SavedLocations = props => {
   const { savedLocations } = props;
   return (
     <>
       <p className="sub-header">Saved Locations</p>
-      {savedLocations.map(location => (
-        <LocationCard
-          key={`saved-location-${location.id}`}
-          location={location}
-          saved
-        />
-      ))}
+      {!!savedLocations ? (
+        savedLocations.map(location => (
+          <LocationCard
+            key={`saved-location-${location.id}`}
+            location={location}
+            saved
+          />
+        ))
+      ) : (
+        <NoLocations className="location-listing">
+          No Saved Locations
+        </NoLocations>
+      )}
     </>
   );
 };
 
 export default SavedLocations;
+
+const NoLocations = styled.p`
+  &&:hover {
+    cursor: default !important;
+    background: #f1ca0957 !important;
+    transform: none !important;
+  }
+`;
